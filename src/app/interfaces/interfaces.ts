536fb1b9a8f6cb09aa1d6852c2f0f846d5a9302f
asincole <asincole@gmail.com>
2022-06-12 15:34:17 +0200

    feat: create interfaces file

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,70 @@
+export interface IUserSummary {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+}
+
+// interface for query results
+export interface IQueryResponse {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Array<IUserSummary>;
+}
+
+// interface for user results
+export interface IUser {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company: string;
+  blog: string;
+  location: string;
+  email: string;
+  hireable: boolean;
+  bio: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface IUserRepository {
+  stargazers_count: number;
+  watchers_count: number;
+  forks: number;
+  html_url: number;
+
+  [key: string]: unknown;
+}
